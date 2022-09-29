ba8d07985116b8aa654de6d99bf444ad905ed410
April Garlington <april.garlington@hotmail.co.uk>
2022-09-29 15:15:05 +0100

    adding items to app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,44 @@ import './App.css';
 
 function App() {
   return (
+    <div>
+      <h1>Welcome to the bakery!</h1>
+
+      <section className="recipes">
+        <h2>Recipes</h2>
+
+        <div>
+          cakes = [
+            {
+              cakeName: "Lemon Drizzle",
+              ingredients: ["eggs", "butter", "lemon  zest", "sugar", "self-raising flour"],
+              rating: 5
+            },
+            
+            {
+              cakeName: "Tea Loaf",
+              ingredients: ["eggs", "oil", "dried fruit", "sugar", "self-raising flour", "strong tea"],
+              rating: 3
+            },
     
+            {
+              cakeName: "Brownie",
+              ingredients: ["chocolate", "eggs", "flour", "butter", "walnuts"],
+              rating: 4
+            },
+    
+            {
+              cakeName: "Carrot Cake",
+              ingredients: ["carrots", "walnuts", "oil", "cream cheese", "flour", "sugar"],
+              rating: 5
+            }
+          ]
+
+          {/* Average rating of all cakes goes here */}
+
+        </div>
+      </section>
+    </div>
   );
 }
 
